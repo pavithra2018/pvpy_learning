78a9bc2370bf85cf07de36e4ab260069c6292a79
bhasvara <bhasvara@cisco.com>
2019-10-15 13:54:16 +0530

    Edited comment

--- any_text_palindrome.py
+++ any_text_palindrome.py
@@ -1,17 +1,18 @@
-#!/Library/Frameworks/Python.framework/Versions/3.6/bin/python -f
-#==================================================================================================
+#
+#===================================================================================
 #
 # NAME        : any_text_palindrome.py
 # DATE        : 15th Oct, 2019
-# DESCRIPTION : A Python program to find if a given text is a Palindrome or not
-# AUTHOR      : Bhaskar Varadaraju
+# DESCRIPTION : A Python program to find, if a given text is a Palindrome or not.
+# AUTHOR      : V Bhaskar
 # VERSION     : 1.0
 #
 # Change History:
-#   VBhaskar    15th Oct, 2019      Created     Added initial version of file
-#   VBhaskar    15th Oct, 2019      Edited      Changed logic to use slicing which is faster.
+#   VBhaskar    15/10/2019  Created     Added initial version of file
+#   VBhaskar    15/10/2019  Edited      Changed logic to use slicing which is faster.
 #
-#==================================================================================================
+#=====================================================================================
+#
 """
     This program is to check if the given text is Palindrome or NOT.
     We reverse the text and compare with original text for equality.
@@ -28,11 +29,10 @@
 
 # Prompt and read text from user
 text_data = input("Enter a value: ")
-# Rereser the given text using slicing and then compare if they are equal
+# Reverse the given text using slicing and compare with original text
 if text_data[::-1] == text_data:
     print(n, "is a Palindrome")
 else:
     print(n, "is NOT a Palindrome")
-
 
 
