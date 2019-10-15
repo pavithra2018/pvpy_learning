6c8b6216ec28763130cce343ab5506cb23fdce77
pavitra_123 <pavitra_123@pavitra>
2019-10-15 08:20:46 +0530

    first n fibonacci series

---
+++ starter_progs/fibanacci.py
@@ -0,0 +1,12 @@
+n = int(input("enter a value "))
+i = 3
+n1 = 0
+n2 = 1
+print("fibonacci series upto given range:")
+print(n1,n2)
+while  i<=n :
+    n3 = n1 + n2
+    print(n3)
+    n1 = n2;
+    n2 = n3;
+    i = i+1;
