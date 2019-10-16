a9e17ea679017827e65e741e3e473293d158bc86
bhasvara <bhasvara@cisco.com>
2019-10-16 16:40:54 +0530

    added more conditions

--- vbr_dir/any_text_palindrome.py
+++ vbr_dir/any_text_palindrome.py
@@ -26,29 +26,50 @@
         5. IF check is true then it is a palindrome
            ELSE it is not a palindrome
 """
+import os
+import sys
+
+LOG_FILENAME = '/tmp/' + str((sys.argv[0]).rsplit('.',1)[0]) + '.log'
+# This is a method to print given message
+# Arguments: 
+#       msg - The message to be printed
+def my_print(msg):
+    print(msg)
+
+## Ensure that only 'bhasvara' user can run this program.
+if os.environ['USER'] != 'bhasvara':
+    my_print("ERROR: This program must be run by 'bhasvara' user only.")
+    exit(1)
 
 # Check if the text is given from Command line interface
-import sys
-if len(sys.argv) == 2:
-    print('Number of cli arguments:', len(sys.argv), 'arguments.')
-    print('Argument List:', str(sys.argv))
+if len(sys.argv) >= 2:
+    #my_print('Number of cli arguments: ' + str(len(sys.argv)) + ' arguments.')
+    #my_print('Argument List: ' + str(sys.argv))
+    my_print('The last argument from CLI was: ' + str(sys.argv[-1]))
+    my_print('Word of text was given via CLI arguments')
+    #my_print(LOG_FILENAME)
     text_data = str(sys.argv[1])
 else:
     # Check if the text is given from environment variable
-    import os
     try:
         text_data = os.environ['TEXT_DATA']
-        print('TEXT_DATA is set in the environment.')
+        my_print('Word of text was given via TEXT_DATA variable in the environment.')
     except KeyError: 
         # Prompt user and read text from console
-        text_data = input('Please enter a text: ')
+        # Ternary operator => text_data = input('Please enter a text: ') if sys.stdin.isatty() else input()
+        if sys.stdin.isatty():
+            text_data = input('Please enter a word of text: ')
+        else:
+            # If we are not having a stdin() attached then read directly without prompy. 
+            #   Ex: PIPE (|) input from echo
+            text_data = input()
     finally:
         # we have the final text_data to validate
-        print(text_data, 'is the text given.')
+        my_print('\"' + text_data + '" is the given word of text.')
 
 # Reverse the given text using slicing and compare with original text
 if text_data[::-1] == text_data:
-    print(text_data, "is a \033[01m\033[34mPalindrome \033[00m")
+    my_print(text_data + ' is a Palindrome')
 else:
-    print(text_data, "is \033[01m\033[34mNOT a Palindrome \033[00m")
+    my_print(text_data + ' is NOT a Palindrome')
 
