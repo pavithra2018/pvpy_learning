6e45e4a99464c175806a8af0892c6aa7b280c702
pavitra_123 <pavitra_123@192.168.1.4>
2019-10-25 15:56:08 +0530

    1. added an argument to find the last element in a list 2. A program to find the number of characters in the given    string

--- cli_prgms.py
+++ cli_prgms.py
@@ -3,6 +3,7 @@ print(argv[0])
 print(argv[1])
 print(argv[2])
 print(argv[3])
-print(argv)
+print(argv)     # to print the arguments in the list
 print(str(argv))
-print(len(argv))
+print(len(argv))# to find no.of command line arguments in the list
+print(argv[-1]) # to find the last element in the list
--- starter_progs/string_characters.py
+++ starter_progs/string_characters.py
@@ -1,5 +1,35 @@
-string = input("enter the string:")
+#
+#===========================================================================================
+#
+#NAME                 : string_characters.py
+#DATE                 : 25th oct, 2019
+#DESCRIPTION          : A python program to find the character count in a given text.
+#AUTHOR               : K pavithra
+#
+#===========================================================================================
+#
+"""
+    This program is to find the character count in agiven text.
+    If any charcter in the text is not equal to space then count 
+    value is incremented.
+    If character is equal to space the loop again itterates 
+    amd finally count value is printed.
+    
+    PSEUDOCODE :
+        1. first read the text from the user
+        2. initialize count to zero
+        3. now itterate a variable through the text
+        4. check if character is equal to space
+        5. if yes continue the itteration
+        6. if no increment count value
+        7. repeat this till the text ends and print the count value
+"""
+
+string = input("enter the text:")
+count = 0
 for x in string :
-    if(x>'a' && x<'z' || x>A && x<Z)
+    if (x != " ") :
         count = count + 1
-        
+print("number of characters in %s is %d" %(string, count))
+
+    
