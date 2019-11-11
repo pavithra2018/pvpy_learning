06d0db4e0d4c440f451ba0b472dc9ecbe4fe6cc7
pavithra2018 <pavithra.konduru2397@gmail.com>
2019-11-11 16:19:00 +0530

    Ok

--- starter_progs/add.py
+++ starter_progs/add.py
@@ -1,4 +1,4 @@
-## Comment 
+## Comment ##
 #print("enter first value")
 val1 = int(input("enter first value:"))
 print("enter second value")
@@ -6,3 +6,5 @@ val2 = int(input())
 print("sum of two numbers is:")
 result = val1 + val2
 print(result)
+
+## End of File ##
