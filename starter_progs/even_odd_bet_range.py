a91b89c48653c8fc1140723e61bcb303908ea97a
pavithra2018 <pavithra.konduru2397@gmail.com>
2019-11-19 15:20:32 +0530

    ok

--- starter_progs/even_odd_bet_range.py
+++ starter_progs/even_odd_bet_range.py
@@ -1,6 +1,15 @@
+"""
+    A program to find the even numbers between the given range using range()
+    
+    syntax of range() :
+        range(start,stop,step)
+        where, stop value is exclusive. 
+"""
+
 num1 = int(input("enter the first value"))
 num2 = int(input("enter the second value"))
-for num in range(num1 , num2 , 1) :
-    if num%2==0 :
-        print(num, end=" ")
+for number in range(num1 , num2 , 1) :
+    if number % 2==0 :
+        print(number, end=" ")
+    
     
